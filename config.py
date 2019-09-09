6ad4322de9cba04d2c46a72eef29128aaeafbc97
lamhoangtung <lamhoangtung.vz@gmail.com>
2019-09-09 11:12:52 +0700

    Improving config file

--- config.py
+++ config.py
@@ -1,23 +1,24 @@
+import os
+
 seed = 0
 
-dataset_name = 'ICDAR2013_ICDAR2017'
+dataset_name = 'datapile'
 test_dataset_name = 'ICDAR2013'
 
-DataLoader_JPN_SYNTH_dataset_path = '/mnt/data/linus/SynthText/results/linus_JPN_2.h5'
+DataLoader_JPN_SYNTH_dataset_path = './linus_JPN_2.h5'
 DataLoader_JPN_SYNTH_Train_Synthesis = '/mnt/data/linus/CRAFT-Remade/logs/train_synthesis/'
 
-
-DataLoaderSYNTH_base_path = '/home/SharedData/Mayank/SynthText/Images'
-DataLoaderSYNTH_mat = '/home/SharedData/Mayank/SynthText/gt.mat'
-DataLoaderSYNTH_Train_Synthesis = '/home/SharedData/Mayank/Models/SYNTH/train_synthesis/'
+# DataLoaderSYNTH_base_path = '/home/SharedData/Mayank/SynthText/Images'
+# DataLoaderSYNTH_mat = '/home/SharedData/Mayank/SynthText/gt.mat'
+# DataLoaderSYNTH_Train_Synthesis = '/home/SharedData/Mayank/Models/SYNTH/train_synthesis/'
 
 DataLoader_Other_Synthesis = '/home/SharedData/Mayank/'+dataset_name+'/Save/'
-Other_Dataset_Path = '/home/SharedData/Mayank/'+dataset_name
+Other_Dataset_Path = os.path.join('./input/', dataset_name)
 save_path = '/home/SharedData/Mayank/Models/WeakSupervision/'+dataset_name
 images_path = '/home/SharedData/Mayank/'+dataset_name+'/Images'
-target_path = '/home/SharedData/Mayank/'+dataset_name+'/Generated'
+target_path = os.path.join('./input', dataset_name, 'generated')
 
-Test_Dataset_Path = '/home/SharedData/Mayank/'+test_dataset_name
+# Test_Dataset_Path = '/home/SharedData/Mayank/'+test_dataset_name
 
 threshold_character = 0.5
 threshold_affinity = 0.5
@@ -39,6 +40,16 @@ dataset_pre_process = {
 		'train': {
 			'target_json_path': None,
 			'target_folder_path': None,
+		},
+		'test': {
+			'target_json_path': None,
+			'target_folder_path': None,
+		}
+	},
+	'datapile': {
+		'train': {
+			'target_json_name': 'train_gt.json',
+			'base_path': './input/datapile/',
 		},
 		'test': {
 			'target_json_path': None,
