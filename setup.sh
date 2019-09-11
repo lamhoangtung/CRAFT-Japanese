f544c1ca7030d7f4f46a3ade3a43aa3ebe7e578f
linus <linus@cinnamon.is>
2019-09-12 00:16:26 +0700

    Add quick setup script

---
+++ setup.sh
@@ -0,0 +1,21 @@
+cd ~
+echo "Cloning the code ..."
+git clone https://github.com/lamhoangtung/CRAFT-Remade
+cd CRAFT-Remade
+echo "Downloading SynthText Japanese dataset for strong supervision ..."
+cd input
+gdrive download 1NcMCuzh1MhgOOQJybxCJcfkNbD_d0w4z
+echo "Downloading Datapile dataset for weak supervision ..."
+gdrive download DATAPILE_DATASET_ZIPED_ID
+unzip datapile.zip
+echo "Downloading pre-trained weight ..."
+cd ../model/
+gdrive download 1be4MtJMEcaolM-s4EMsCRUmJFg2pR2OI
+gdrive download 1ZQE0tK9498RhLcXwYRgod4upmrYWdgl9
+echo "Installing dependencies ..."
+pip3 install -r requirements.txt
+echo "Preprocessing dataset ..."
+python3 main.py pre-process --dataset datapile
+echo "Everthing is almost done. Check all of yours config and start training weak supervision with this command:"
+echo "$ python3 main.py weak-supervision --model ./model/craft_mlt_25k.pth --iterations 10"
+echo "Or read the README for more"
